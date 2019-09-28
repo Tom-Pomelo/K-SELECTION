8749d168d48561f8309acd76ae3b9f3b4a3ea854
Tom-Pomelo <jixingyou@sjtu.edu.cn>
2019-09-27 23:00:24 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/K_SELECTION.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/K_SELECTION.dir/MAIN.c.o"
+  "K_SELECTION.pdb"
+  "K_SELECTION"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/K_SELECTION.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ doalg.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int doalg(int n, int k, int *Best) {
+    return 1;
+}
+
+//int partition(int n, int k, int )
